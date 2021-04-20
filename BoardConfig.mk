b1e6cd716645698f3a1f85264d58f7783159d48f
kytoaq <kytoaq@gmail.com>
2021-04-19 22:22:38 -0500

    doha: Drop all configurations for FOD Service
    Ok, I have no idea why I did not remove this before, but analyzing
    more in depth it is evident that this device does not have any
    relationship with FOD service, so it was removed.
    
    Continuing with the commit racer> doha I remove the parts where
    makes mention.
    
    Signed-off-by: kytoaq <kytoaq@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,10 +21,6 @@ DEVICE_PATH := device/motorola/doha
 # Display
 TARGET_SCREEN_DENSITY := 420
 
-# FOD
-TARGET_SURFACEFLINGER_FOD_LIB := //$(DEVICE_PATH):libfod_extension.doha
-TARGET_USES_FOD_ZPOS := true
-
 # FM
 BOARD_HAVE_QCOM_FM := true
 BOARD_HAS_QCA_FM_SOC := "cherokee"
