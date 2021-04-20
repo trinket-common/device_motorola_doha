38a4847fcb25d3da186900f6aa350f6cb6e21bcb
kytoaq <kytoaq@gmail.com>
2021-04-19 22:30:27 -0500

    doha: Commonize FM Radio
    Signed-off-by: kytoaq <kytoaq@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,10 +21,6 @@ DEVICE_PATH := device/motorola/doha
 # Display
 TARGET_SCREEN_DENSITY := 420
 
-# FM
-BOARD_HAVE_QCOM_FM := true
-BOARD_HAS_QCA_FM_SOC := "cherokee"
-
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
--- properties.mk
+++ properties.mk
@@ -6,10 +6,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.physical.num=4
 
-# FM
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.fm.use_audio_session=true
-
 # Firmware
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.hw.modem_version=.
